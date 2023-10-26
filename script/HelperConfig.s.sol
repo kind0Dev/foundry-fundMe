af3e869909566dd5f76ce3233f6c5b7f3b374b26
kaindHub <rahj0881@gmail.com>
2023-10-26 04:01:11 -0700

    first_commit

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT
+
+//  1. Deploy mocks when we are on a local anvil chain
+// 2. keep track of contract address across different chains
+// sepolia eth/usd
+// mainnet eth/usd
+
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/Mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script {
+    // if we are on a local anvil, we deploy mocks
+    // otherwise, grab the exiting address from the live network
+
+    NetworkConfig public activeNetworkConfig;
+
+    uint8 public constant DECIMALS = 8;
+    int256 public constant INITIAL_PRICE = 2000e8;
+
+    struct NetworkConfig {
+        address priceFeed; //eth/usd price feed address
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        //price feed address
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        //price feed address
+        NetworkConfig memory mainnetConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return mainnetConfig;
+    }
+
+    function getOrCreateAnvilEthConfig() public returns (NetworkConfig memory) {
+        if (activeNetworkConfig.priceFeed != address(0)) {
+            return activeNetworkConfig;
+        }
+
+        //1. DEPLOY THE MOCKS
+        //2. RETURN THE MOCK ADDRESS
+
+        vm.startBroadcast();
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(
+            DECIMALS,
+            INITIAL_PRICE
+        );
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({
+            priceFeed: address(mockPriceFeed)
+        });
+        return anvilConfig;
+    }
+}
---
+++ script/Interactions.s.sol
@@ -0,0 +1,51 @@
+// SPDX-License-Identifier: MIT
+
+//fund
+// withdraw
+pragma solidity ^0.8.18;
+
+import {Script, console} from "forge-std/Script.sol";
+import {DevOpsTools} from "@foundry-devops/DevOpsTools.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundFundMe is Script {
+    uint256 constant SEND_VALUE = 0.01 ether;
+
+    function fundFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).fund{value: SEND_VALUE}();
+        vm.stopBroadcast();
+        console.log("funded fundme with %s", SEND_VALUE);
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        vm.startBroadcast();
+        FundFundMe(mostRecentlyDeployed);
+        vm.stopBroadcast();
+    }
+}
+
+contract WithdrawFundMe is Script {
+    uint256 constant SEND_VALUE = 0.1 ether; // 100000000000000000
+
+    function withdrawFundMe(address mostRecentlyDeployed) public {
+        vm.startBroadcast();
+        FundMe(payable(mostRecentlyDeployed)).withdraw();
+        vm.stopBroadcast();
+        console.log("funded fundme with %s", SEND_VALUE);
+    }
+
+    function run() external {
+        address mostRecentlyDeployed = DevOpsTools.get_most_recent_deployment(
+            "FundMe",
+            block.chainid
+        );
+        vm.startBroadcast();
+        withdrawFundMe(mostRecentlyDeployed);
+        vm.stopBroadcast();
+    }
+}
